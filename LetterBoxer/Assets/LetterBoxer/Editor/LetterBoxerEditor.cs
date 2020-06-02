08e1c00d948cc7f4d17c6189d6550a349c8ec35b
rabidgremlin <jack@rabidgremlin.com>
2020-06-02 19:27:54 +1200

    Initial check in.

---
+++ LetterBoxer/Assets/LetterBoxer/Editor/LetterBoxerEditor.cs
@@ -0,0 +1,58 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(LetterBoxer))]
+public class LetterBoxerEditor : Editor
+{
+    SerializedProperty referenceModeProp;
+    SerializedProperty xProp;
+    SerializedProperty yProp;
+    SerializedProperty widthProp;
+    SerializedProperty heightProp;
+    SerializedProperty onAwakeProp;
+    SerializedProperty onUpdateProp;
+    
+
+    void OnEnable()
+    {
+        referenceModeProp = serializedObject.FindProperty("referenceMode");
+        xProp = serializedObject.FindProperty("x");
+        yProp = serializedObject.FindProperty("y");
+        widthProp = serializedObject.FindProperty("width");
+        heightProp = serializedObject.FindProperty("height");
+        onAwakeProp = serializedObject.FindProperty("onAwake");
+        onUpdateProp = serializedObject.FindProperty("onUpdate");
+    }
+
+    override public void OnInspectorGUI()
+    {
+        serializedObject.Update();        
+        EditorGUILayout.PropertyField(referenceModeProp, new GUIContent("Reference Mode", "Toggles whether you want to use an aspect ratio or a resolution to calculate the letterboxing"));
+
+        LetterBoxer.ReferenceMode currentReferenceMode = (LetterBoxer.ReferenceMode)referenceModeProp.enumValueIndex;
+
+        if (currentReferenceMode == LetterBoxer.ReferenceMode.DesignedAspectRatio)
+        {
+            EditorGUI.indentLevel++;            
+            EditorGUILayout.PropertyField(xProp, new GUIContent("X", "The X portion of the aspect ratio"));
+            EditorGUILayout.PropertyField(yProp, new GUIContent("Y", "The Y portion of the aspect ratio"));            
+            EditorGUI.indentLevel--;
+        }
+        else
+        { 
+            EditorGUI.indentLevel++;
+            EditorGUILayout.PropertyField(widthProp, new GUIContent("Width", "The width in pixels"));
+            EditorGUILayout.PropertyField(heightProp, new GUIContent("Height", "The height in pixels"));
+            EditorGUI.indentLevel--;            
+        }
+                
+        EditorGUILayout.PrefixLabel("Calculate");
+        EditorGUI.indentLevel++;
+        EditorGUILayout.PropertyField(onAwakeProp, new GUIContent("On Awake", "Calculate the letterboxing during OnAwake()"));
+        EditorGUILayout.PropertyField(onUpdateProp, new GUIContent("On Update", "Calculate the letterboxing during OnUpdate()"));
+        EditorGUI.indentLevel--;
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+
---
+++ LetterBoxer/Assets/LetterBoxer/LetterBoxer.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LetterBoxer : MonoBehaviour
+{    
+    public enum ReferenceMode { DesignedAspectRatio, OrginalResolution };
+  
+    public ReferenceMode referenceMode; 
+    public float x=16;
+    public float y=9;  
+    public float width = 960;
+    public float height = 540;
+    public bool onAwake = true;
+    public bool onUpdate = true;
+
+    private Camera cam;        
+
+    public void Awake()
+    {
+        // store reference to the camera
+        cam = GetComponent<Camera>();
+
+        // perform sizing if onAwake is set
+        if(onAwake)
+        {
+            PerformSizing();
+        }
+    }
+
+    public void Update()
+    {
+        // perform sizing if onUpdate is set
+        if (onUpdate)
+        {
+            PerformSizing();
+        }
+    }
+
+    private void OnValidate()
+    {
+        x = Mathf.Max(1, x);
+        y = Mathf.Max(1, y);
+        width = Mathf.Max(1, width);
+        height = Mathf.Max(1, height);
+    }
+
+    // based on logic here from http://gamedesigntheory.blogspot.com/2010/09/controlling-aspect-ratio-in-unity.html
+    private void PerformSizing()
+    {
+        // calc based on aspect ratio
+        float targetRatio = x / y;
+
+        // recalc if using resolution as reference
+        if (referenceMode == LetterBoxer.ReferenceMode.OrginalResolution)
+        {
+            targetRatio = width / height;
+        }
+
+        // determine the game window's current aspect ratio
+        float windowaspect = (float)Screen.width / (float)Screen.height;
+
+        // current viewport height should be scaled by this amount
+        float scaleheight = windowaspect / targetRatio;
+
+        // if scaled height is less than current height, add letterbox
+        if (scaleheight < 1.0f)
+        {
+            Rect rect = cam.rect;
+
+            rect.width = 1.0f;
+            rect.height = scaleheight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleheight) / 2.0f;
+
+            cam.rect = rect;
+        }
+        else // add pillarbox
+        {
+            float scalewidth = 1.0f / scaleheight;
+
+            Rect rect = cam.rect;
+
+            rect.width = scalewidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scalewidth) / 2.0f;
+            rect.y = 0;
+
+            cam.rect = rect;
+        }
+    }
+}
