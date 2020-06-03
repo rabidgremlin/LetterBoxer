3e18c1a80c03f96c3e2b9af8a0d72184d94b4b02
rabidgremlin <jack@rabidgremlin.com>
2020-06-04 10:00:49 +1200

    Added documentation and new demo scene.

---
+++ LetterBoxer/Assets/LetterBoxer/Demo/Scripts/PingPongMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPongMove : MonoBehaviour {
+
+    public Transform a, b;
+    [Range(0, 1)]
+    public float speed = 1;
+
+    private Vector3 lastPos;
+    public bool faceMovementDirection = false;
+
+    private void Start()
+    {
+        lastPos = transform.position;
+    }
+
+    void Update()
+    {
+        float pingPong = Mathf.PingPong(Time.time * speed, 1);
+        transform.position = Vector3.Lerp(a.position, b.position, pingPong);
+
+        if (faceMovementDirection)
+        {
+            if (lastPos.x > transform.position.x )
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+            else
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+
+            lastPos = transform.position;
+        }
+
+    }
+}
---
+++ LetterBoxer/Assets/LetterBoxer/Demo/Scripts/TrackMouse.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackMouse : MonoBehaviour {
+
+    public GameObject bulletHole;
+
+    private int hitLayeMask;
+
+    // Use this for initialization
+    void Start () {
+        Cursor.visible = false;
+        hitLayeMask = LayerMask.GetMask(new string[] { "Target", "Decoy", "Background" });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cursor.visible = false;
+        Vector3 pos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1);
+        pos = Camera.main.ScreenToWorldPoint(pos);
+        transform.position = pos;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Collider2D hitColider = Physics2D.OverlapPoint(pos, hitLayeMask);
+
+            if (hitColider != null)
+            {
+                GameObject newHole =  Instantiate(bulletHole, transform.position, transform.rotation);
+                newHole.transform.parent = hitColider.transform;
+
+                int sortorder = (hitColider.transform.GetComponent<SpriteRenderer>()).sortingOrder;
+                (newHole.transform.GetComponent<SpriteRenderer>()).sortingOrder = sortorder+1;
+                Destroy(newHole, 5);
+            }            
+        }
+    }
+}
